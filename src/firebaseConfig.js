1739e17c252d401b55f812fb40d95908cae5c0de
ShriLingam23 <trnshri23@gmail.com>
2019-07-06 00:50:09 +0530

    Basic implementation

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyANkRVJ_a97_z_ULGKmhWysYxF_QdRaCVw",
+    authDomain: "djamware-51bcd.firebaseapp.com",
+    databaseURL: "https://djamware-51bcd.firebaseio.com",
+    projectId: "djamware-51bcd",
+    storageBucket: "djamware-51bcd.appspot.com",
+    messagingSenderId: "686658768199",
+    appId: "1:686658768199:web:70c859b7db511c31"
+};
+
+export default firebaseConfig;
